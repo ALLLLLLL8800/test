594d2e48c5991c090eeb5f41ce51ac68d332d03a
ALLLLLLL8800 <aliajam1375@gmail.com>
2026-05-22 09:45:52 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+import requests
+TOKEN = "1825880479:SKAT3qpdRSp5gtx1YYvmR_hgR4TvvQUNN2U"
+CHAT_ID = "5227164458"
+r = requests.post(f"https://tapi.bale.ai/bot{TOKEN}/sendMessage", json={"chat_id": CHAT_ID, "text": "Test"})
+print(r.status_code, r.text)
